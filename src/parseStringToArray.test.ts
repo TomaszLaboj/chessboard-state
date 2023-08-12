704ec9623513651e76713f262496fc00a0773da8
TomaszLaboj <tomasz.laboj@gmail.com>
2023-08-12 11:39:29 +0100

    added tests

--- src/parseStringToArray.test.ts
+++ src/parseStringToArray.test.ts
@@ -1,4 +1,5 @@
-import { parseStringToArray } from "./parseStringToArray";
+import { parseStringToArray, checkForCorrectCharacters } from "./parseStringToArray";
+
 
 test("to test parsing input string into array",() => {
 
@@ -12,6 +13,33 @@ test("to test parsing input string into array",() => {
     R N B Q K B N R
     `)).toEqual(["r","n","b","q","k","b","n","r","p","p","p","p","p","p","p","p",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".","P","P","P","P","P","P","P","P","R","N","B","Q","K","B","N","R"])
 })
+test("to test parsing input string into array",() => {
+
+    expect(parseStringToArray(`rnkbppp..`)).toEqual(["r","n","k","b","p","p","p",".","."])
+})
+
+test("test for incorrect characters",() => {
+
+    expect(checkForCorrectCharacters(`rnkbppp..`)).toEqual([])
+})
+test("test for incorrect characters",() => {
+
+    expect(checkForCorrectCharacters(`rnkbppp.d.o`)).toEqual(["d","o"])
+})
+
+test("check for containing only correct chars",() => {
+
+    expect(checkForCorrectCharacters(`r n b q k b n r
+    p p p p p p p p
+    . . . . . . . .
+    . . . . . . . .
+    . . . . . . . .
+    . . . . . . . .
+    P P P P P P P P
+    R N B Q K B N R
+    `)).toEqual([])
+});
+
 test("to test parsing input string into array",() => {
 
     expect(parseStringToArray(`rnkbppp..`)).toEqual(["r","n","k","b","p","p","p",".","."])
--- src/parseStringToArray.ts
+++ src/parseStringToArray.ts
@@ -1,4 +1,17 @@
 export function parseStringToArray(boardStateString:string):string[]{
+
     const boardStringAsArray:string[] = boardStateString.replace(/[' ']/g,'').split('').filter((char) => char !== '\n')
      return boardStringAsArray;
  }
+
+ export function checkForCorrectCharacters(boardStateString:string):string[]{
+    const incorrectChars:string[] = [];
+    const correctChars: string[] = ["r","n","b","k","q","p",".","R","N","B","K","Q","P"," ","\n"]
+    for (const char of boardStateString){
+        if(!correctChars.includes(char)){
+            incorrectChars.push(char)
+        }
+    }
+    return incorrectChars;
+ } 
+
